eff205c69228d50cd226ad87e8597f1b3954b33c
anasou <anasounatural@gmail.com>
2023-12-19 17:43:17 -0300

    backupgit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const textarea = document.querySelector('[name="user-input"]');
+elemento = document.getElementById("container")
