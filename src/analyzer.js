93d4a56682e54ae14ce2a1a468bd4dab068a12f6
anasou <anasounatural@gmail.com>
2024-01-10 15:05:46 -0300

    Atualizações pré fim de projeto

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,22 +1,52 @@
-const analyzer = {  
+
+const analyzer = {
   getWordCount: (text) => {
-    //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    const words = text.split(/\s+/).filter(palavra => palavra !== ""); //variável const word pega o (text) declarado no js, divide em palavras e retira espaços vazios. Split= tira espaços. Filter= filtrar filtrar no texto e colocar num nova caixa tudo que atender a condição dentro de (), que neste caso é pegar strings que nao sejam vazias.//= delimita Regex. \s= espaços. += o anterior \s será repetido. !== significa diferente/opost. "" vazio
+    return words.length;
   },
+
+
   getCharacterCount: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    return text.length; // aqui não precisa criar variável como palavras na L5, pq o padrão de entendimento da máquina é em caracteres
   },
+
   getCharacterCountExcludingSpaces: (text) => {
-    //TODO: esta función debe retornar el recuento de caracteres excluyendo espacios y signos de puntuación que se encuentran en el parámetro `text` de tipo `string`.
+    return text.replace(/[^\w]/g,'').length; // sintaxe string.replace(tira, adiciona)  substitua o que está antes da vírgula pelo que está depois da vírgula. ^ = negação.  \w = engloba [a-zA-Z0-9_]. g = em todo documento
   },
-  getAverageWordLength: (text) => {    
-    //TODO: esta función debe retornar la longitud media de palabras que se encuentran en el parámetro `text` de tipo `string`.
+
+  getAverageWordLength: (text) => {
+    const words = text.split(' ');//split: divida (text) onde vc encontrar (' ') espaço em branco. Tb poderia ser onde encontrar vírgula ou etc. + Usa Const pq não vai variar o que quero fazer com text
+    let totalWords = 0 // Declara que quer começar contagem do total de palavras a partir do zero  Usa let pq isso depois vai mudar
+    for (let i = 0; i < words.length; i++) // For é um loop com parâmetro (posição inicial; condição; incrementar +1)
+      totalWords += words[i].length // pegue o que está no lado esquerdo de += some com a quantidade de intens de word na posição 0
+    return parseFloat((totalWords / words.length).toFixed(2)); //parseFloat retorna numeros decimais e o to Fixed para fixar casas deciamis em 2. 
   },
+
   getNumberCount: (text) => {
-    //TODO: esta función debe retornar cúantos números se encuentran en el parámetro `text` de tipo `string`.
+    const listaNumeros = text.match(/\b\d+(\.\d+)?\b/g); // string.match(regex) match busca na string o padrão anunciado no regex 
+    if (listaNumeros) {
+      return listaNumeros.length;
+    }
+    else {
+      return 0;
+    }
   },
+
   getNumberSum: (text) => {
-    //TODO: esta función debe retornar la suma de todos los números que se encuentran en el parámetro `text` de tipo `string`.
+    const listaNumeros = text.match(/\b\d+(\.\d+)?\b/g); // regex para pegar numeros interios ou decimais. qdeclara \b???? \d+ um ou mais digitos; (\.\d+) com 2 ou mais numeros decimais; ? opcional, ou seja tb pode ser inteiro
+
+    if (listaNumeros) {
+      let resultado = 0 //diz que o padrao inicial de acumular a soma dos numeros é 0
+      for (let i = 0; i < listaNumeros.length; i++) {//FOR percorre numeros da lista e segura / i++, i=1+1, i+=1 adiciona à uma "caixa" os números encontrados
+        resultado += Number(listaNumeros[i]); // outra forma de escrever a linha  acima //Numer pegou todas as stings tranformou em numero e + (somou) e =(deu o resultado) 
+        //resultado = resultado + Number(listaNumeros[i]);  outra forma de escrever a linha  acima//Number transforma string em numeros a serem somados // igual a linha 59, mas extendida
+      }
+      return resultado;
+    }
+    else return 0;
+
   },
+
 };
 
 export default analyzer;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,62 @@
 import analyzer from './analyzer.js';
-
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+//DECLARAÇÕES 
+const textArea = document.querySelector('[name="user-input"]'); // Pego o atributo name="user-input" no html e declaro com nome textarea
+const button = document.getElementById("reset-button");
+const wordCount = document.querySelector('[data-testid="word-count"]');
+const characterCount = document.querySelector('[data-testid="character-count"]');
+const characterNoSpacesCount = document.querySelector('[data-testid="character-no-spaces-count"]');
+const numberCount = document.querySelector('[data-testid="number-count"]');
+const numberSum = document.querySelector('[data-testid="number-sum"]');
+const wordLengthAverage = document.querySelector('[data-testid="word-length-average"]');
 
-const textarea = document.querySelector('[name="user-input"]');
-elemento = document.getElementById("container")
-oninput
+//ADIÇÃO DE EVENTOS
+textArea.addEventListener("input", metricas); //Escuta a função padrão "keyup" e pede que realiza a função que nomeei metricas e explico abaixo, Obs no Redme atual pediu p substituir por input
+button.addEventListener("click", resetar);
+
+//DECLARAÇÃO DA FUNÇÕES DOS EVENTOS
+function resetar() {
+  textArea.value = ''; //Faz apagar o text area - manipulacao visual x lógica
+  wordCount.textContent = "Contagem de palavras: 0"; // Por que não só ="0"
+  characterCount.textContent = "Contagem de Caracteres: 0";
+  characterNoSpacesCount.textContent = "Contagem de Caracteres Sem Espaços: 0";
+  numberCount.textContent = "Contagem de Numeros: 0";
+  numberSum.textContent = "Soma de Numeros: 0";
+  wordLengthAverage.textContent = "Tamanho Medio das Palavras: 0";
+}
+
+function metricas() {
+  const text = textArea.value; //Text area é o quadrado e value é o digitado no quadrado
+  wordCount.textContent = "Contagem de palavras: " + analyzer.getWordCount(text);
+  characterCount.textContent = "Contagem de Caracteres: " + analyzer.getCharacterCount(text);
+  characterNoSpacesCount.textContent = "Contagem de Caracteres sem Espaços: " + analyzer.getCharacterCountExcludingSpaces(text);
+  numberCount.textContent = "Contagem de Numeros:" + analyzer.getNumberCount(text);
+  numberSum.textContent = "Soma de Numeros:" + analyzer.getNumberSum(text);
+  wordLengthAverage.textContent = "Tamanho Medio das Palavras:" + analyzer.getAverageWordLength(text);
+
+}
+
+//CONEXÃO DE AÇÃO COM ANALYZER
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* MINHA PRIMEIRA TENTATIVA
+const resetbutton = document.getElemetById('reset-button'); //Declarando no js um elemento de id buscado no html
+const textarea = document.querySelector("user-input"); //Declrando no js um elemento de tag buscado no html
+
+resetbutton.addEventListener(click, funciton[textarea.value = '']); //apaga a text area - qual nome da sintaxe desse textearea.value.
+textarea.addEventListener(input, function (){ })
+*/
+
